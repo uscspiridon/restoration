5582a8bbda7b957d0e516c81c59cbc0594d0d6e8
Colin Spiridonov <spiridon@usc.edu>
2021-11-05 22:32:34 -0700

    Molds randomly multiply

---
+++ Assets/Scripts/Multiply.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiply : MonoBehaviour {
+    public GameObject moldPrefab;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+
+    private float spawnTime;
+    private float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTimer();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime;
+        if (timer >= spawnTime) {
+            // determine random position
+            Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+            randomDirection.Normalize();
+            float radius = 0.5f;
+            randomDirection *= radius;
+            Vector3 spawnPosition = transform.position + randomDirection;
+            Debug.Log("spawnPosition = (" + spawnPosition.x + ", " + spawnPosition.y + ")  randomDirection = " + randomDirection.x + ", " + randomDirection.y + ")");
+            // instantiate the prefab
+            Instantiate(moldPrefab, spawnPosition, Quaternion.identity);
+            // reset the timer
+            ResetTimer();
+        }
+    }
+
+    private void ResetTimer() {
+        timer = 0f;
+        spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+}
